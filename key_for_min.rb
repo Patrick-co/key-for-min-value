89dda1d01363e21e23f2bd3a86e32dd3e5735c9f
Patrick <patricktaylec@gmail.com>
2021-02-06 15:44:49 -0500

    commit tree 56bafb984b300ec5448afadd3bff42d281ad6b72

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,6 +1,10 @@
 # prereqs: iterators, hashes, conditional logic
 # Given a hash with numeric values, return the key for the smallest value
+# hash = {:blake => 500, :ashley => 2, :adam => 1}
 
-def key_for_min_value(name_hash)
+hash = {:blake => 500, :ashley => 2, :adam => 1}
+hash = {:blake => 10, :ashley => 50, :adam => 17}
 
-end
+def key_for_min_value(hash)
+    hash.inject { |m,a| m[1] > a[1] ? a : m }[0] unless hash.empty?
+  end
